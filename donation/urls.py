0b44202579a03134ced067b17effd319c8d881a8
Moaaj <moaaj.upm@gmail.com>
2025-06-28 13:25:10 +0800

    Add main project files, templates, static assets, and localization

---
+++ donation/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/create-donation-event/', views.create_donation_event, name='create_donation_event'),
+] 
